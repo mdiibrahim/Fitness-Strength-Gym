b4d5209ec78dfc054d78fc9af9c67c4d5c517c82
mdiibrahim <mdiibrahim549@gmail.com>
2022-10-03 13:15:40 +0600

    google font added

---
+++ src/Components/Main-Container/MainContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainContainer = () => {
+    return (
+        <div>
+            <p>hello from main conatiner</p>
+        </div>
+    );
+};
+
+export default MainContainer;
