1ff2628bc243e0da47c4c94e90d8cc62bba5ac5c
mdiibrahim <mdiibrahim549@gmail.com>
2022-10-03 13:50:37 +0600

    title are added in ui and tab

--- src/Components/Main-Container/MainContainer.js
+++ src/Components/Main-Container/MainContainer.js
@@ -1,9 +1,22 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDumbbell } from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
+import './MainContainer.css'
 
 const MainContainer = () => {
     return (
-        <div>
-            <p>hello from main conatiner</p>
+        <div className='Main-container'>
+            <div className='Exercise-container'>
+                <div className='Title'>
+                    <FontAwesomeIcon icon={faDumbbell} className='Fw-icon'></FontAwesomeIcon>
+                    <h1>Fitness Strength Gym</h1>
+                </div>
+
+                <p>Today's exercise plan is: </p>
+            </div>
+            <div className='Profile-container'>
+                <p>hello</p>
+            </div>
         </div>
     );
 };
