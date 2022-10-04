35920fc7fef96ff6072adf897ec2f21e931ef988
mdiibrahim <mdiibrahim549@gmail.com>
2022-10-04 21:53:22 +0600

    exercise time are fixed in profile

--- src/App.js
+++ src/App.js
@@ -7,57 +7,58 @@ import Profile from './Components/Main-Container/Profile/Profile';
 
 
 function App() {
-  const LoadExercises = () => {
-    const [exercises, setExercises] = useState([]);
-    useEffect(() => {
-        fetch('data.json')
-            .then(res => res.json())
-            .then(data => setExercises(data))
-    }, [])
-    const DoExercise = (time) => {
-      
-    console.log(time)
-    
+    const LoadExercises = () => {
+        const [exercises, setExercises] = useState([]);
+        useEffect(() => {
+            fetch('data.json')
+                .then(res => res.json())
+                .then(data => setExercises(data))
+        }, [exercises])
+        const DoExercise = (time) => {
+            //from Profile.js, exercise time are get
+            const previousExerciseTime = +(document.getElementById('previous-exercise-time').innerText);
+            const newExerciseTime = previousExerciseTime + time;
+            document.getElementById('previous-exercise-time').innerText = newExerciseTime;
+        }
+        return (
+            <div className='ExerciseList-container' >
+                {
+                    exercises.map(Exercise => <ExerciseList
+                        id={Exercise.id}
+                        key={Exercise.id}
+                        img={Exercise.img}
+                        name={Exercise.name}
+                        level={Exercise.level}
+                        time={Exercise.time}
+                        about={Exercise.about}
+                        DoExercise={DoExercise}
+                    ></ExerciseList>)
+                }
+
+            </div>
+
+        )
     }
+
     return (
-        <div className='ExerciseList-container' >
-            {
-                exercises.map(Exercise => <ExerciseList
-                    id={Exercise.id}
-                    key={Exercise.id}
-                    img={Exercise.img}
-                    name={Exercise.name}
-                    level={Exercise.level}
-                    time={Exercise.time}
-                    about={Exercise.about}
-                    DoExercise={DoExercise}
-                ></ExerciseList>)
-            }
+        <div className="App">
+            <div className='Main-container'>
+                <div className='Exercise-container'>
 
-        </div>
-
-    )
-  }
-
-  return (
-    <div className="App">
-      <div className='Main-container'>
-            <div className='Exercise-container'>
-
-                <div className='Title'>
-                    <FontAwesomeIcon icon={faDumbbell} className='Fw-icon'></FontAwesomeIcon>
-                    <h1>Fitness Strength Gym</h1>
-                </div>
+                    <div className='Title'>
+                        <FontAwesomeIcon icon={faDumbbell} className='Fw-icon'></FontAwesomeIcon>
+                        <h1>Fitness Strength Gym</h1>
+                    </div>
 
                     <p>Today's exercise plan is: </p>
                     <LoadExercises></LoadExercises>
-            </div>
-            <div className='Profile-container'>
-                <Profile></Profile>
+                </div>
+                <div className='Profile-container'>
+                    <Profile></Profile>
+                </div>
             </div>
         </div>
-    </div>
-  );
+    );
 }
 
 
